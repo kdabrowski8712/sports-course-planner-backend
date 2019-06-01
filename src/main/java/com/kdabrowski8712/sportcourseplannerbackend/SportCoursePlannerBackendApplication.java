448934d3bb3967b9461af7159e667bc4f54e2927
k.dabrowski8712 <k.dabrowski8712@gmail.com>
2019-06-01 10:18:55 +0200

    initial commit

---
+++ src/main/java/com/kdabrowski8712/sportcourseplannerbackend/SportCoursePlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kdabrowski8712.sportcourseplannerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportCoursePlannerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportCoursePlannerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kdabrowski8712/sportcourseplannerbackend/SportCoursePlannerBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kdabrowski8712.sportcourseplannerbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportCoursePlannerBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
